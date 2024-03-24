fb743709c96864c42e1df0490c03c6c30fff768f
FeHunter <hunterfe1111@gmail.com>
2024-03-24 18:43:04 -0300

    Adição do icone de carregamento em mais telas

--- screens/Tabs/ProductGeral.js
+++ screens/Tabs/ProductGeral.js
@@ -1,19 +1,32 @@
-import {View, Text, Image, StyleSheet, Pressable} from 'react-native';
+import {View, Text, Image, StyleSheet, ActivityIndicator} from 'react-native';
 import Routes from '../../assets/Routes';
+import { useEffect, useState } from 'react';
 
 export function ProductGeral ({route}){
     const { product } = route.params;
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(()=>{
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1500);
+    },[]);
 
     return (
-        <View style={styles.card}>
-            <View style={styles.header}>
-                <Text style={styles.title}>{product.nome}</Text>
-                <Text style={styles.preco}>R${product.preco}</Text>
+        <View style={{width: '100%'}}>
+            {isLoading ?
+                <ActivityIndicator size="large" color="#0000ff" />
+            :
+            <View style={styles.card}>
+                <View style={styles.header}>
+                    <Text style={styles.title}>{product.nome}</Text>
+                    <Text style={styles.preco}>R${product.preco}</Text>
+                </View>
+                <View style={styles.body}>
+                    <Image source={{uri: product.imagens[0]}} style={styles.imagem} />
+                    <Text style={styles.descricao}>{product.descricao}</Text>
+                </View>
             </View>
-            <View style={styles.body}>
-                <Image source={{uri: product.imagens[0]}} style={styles.imagem} />
-                <Text style={styles.descricao}>{product.descricao}</Text>
-            </View>
+            }
         </View>
     );
 }
--- screens/Tabs/ProductImage.js
+++ screens/Tabs/ProductImage.js
@@ -1,18 +1,29 @@
-import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, Image, FlatList, StyleSheet, ActivityIndicator } from 'react-native';
 
 export function ProductImage({ route }) {
     const { product } = route.params;
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(()=>{
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1500);
+    },[]);
     return (
         <View style={styles.container}>
             <Text style={styles.title}>{product.nome}</Text>
-            <FlatList
-                style={{width: '100%', height: 800}}
-                data={product.imagens}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={({item}) => {
-                    return <Image source={{ uri: item }} style={styles.image} />
-                }}
-            />
+            {isLoading ?
+                <ActivityIndicator size="large" color="#0000ff" />
+            :
+                <FlatList
+                    style={{width: '100%', height: 800}}
+                    data={product.imagens}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({item}) => {
+                        return <Image source={{ uri: item }} style={styles.image} />
+                    }}
+                />
+            }
         </View>
     );
 }
